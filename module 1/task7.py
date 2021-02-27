26006bfc5349ba875abada85a8a40ca70cafe2d8
Mikhail <newlevelcenter@gmail.com>
2021-02-27 14:46:50 +0200

    task7

---
+++ module 1/task7.py
@@ -0,0 +1,10 @@
+import math
+x1 = int(input("Введите х1\n"))
+y1 = int(input("Введите y1\n"))
+x2 = int(input("Введите х2\n"))
+y2 = int(input("Введите y2\n"))
+# Ввод данных
+
+l = math.sqrt(pow(x1-x2,2)+pow(y1-y2,2))
+
+print(l)
